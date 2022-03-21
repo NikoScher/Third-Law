06476b9b45d0fa52a991d5576f4b2a3e9c4365c6
Niko Scher <niko.scher.26@gmail.com>
2022-03-21 23:58:49 +0000

    Jank spinning lmao but hey progress?

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Fields
+    [SerializeField] Transform holdTransform;
+    List<Rigidbody> entityList = new List<Rigidbody>();
+    Rigidbody heldEntity = null;
+
+    Rigidbody rb;
+
+    public float launchFor = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Rigidbody entity;
+        if(collider.GetComponent<Rigidbody>() != null) {
+            entity = collider.GetComponent<Rigidbody>();
+            Rigidbody checkEntity = entityList.Find(x=> x == entity);
+            if (checkEntity == null)
+                entityList.Add(entity);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        Rigidbody entity = collider.GetComponent<Rigidbody>();
+        entityList.Remove(entity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(temp);
+        Vector3 targetVec = mousePos - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetVec), 0.5f);
+
+        if(heldEntity != null) {
+            Vector3 smoothPose = Vector3.Lerp(heldEntity.position, holdTransform.position, 0.5f);
+            heldEntity.MovePosition(smoothPose);
+        }
+
+        if (Input.GetButtonDown("Fire1") && heldEntity != null) {
+            heldEntity.isKinematic = false;
+            Vector3 forceVec = holdTransform.transform.forward.normalized * heldEntity.mass * launchFor;
+            heldEntity.AddForce(forceVec, ForceMode.VelocityChange);
+            heldEntity = null;
+            rb.AddForce(-forceVec, ForceMode.VelocityChange);
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire2")) {
+            if(heldEntity != null) {
+                heldEntity.isKinematic = false;
+                heldEntity = null;
+                return;
+            }
+            if (entityList.Count != 0) {
+                float minDist = float.MaxValue;
+                foreach (Rigidbody entity in entityList) {
+                    Vector3 entityPos = entity.GetComponent<Transform>().position;
+                    Vector3 dispVec = entityPos - transform.position;
+                    if (dispVec.magnitude < minDist) {
+                        heldEntity = entity;
+                        minDist = dispVec.magnitude;
+                    }
+                }
+                heldEntity.isKinematic = true;
+            }
+        }
+    }
+
+}
